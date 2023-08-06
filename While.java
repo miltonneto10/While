5d76cea29ad280a9e41b6d46e4209355163b4e17
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-06 12:20:19 -0300

    Commit Inicial

---
+++ While.java
@@ -0,0 +1,20 @@
+//FLUXO DE REPETIÇÃO
+//Utilização do WHILE (ENQUANTO)
+	
+import java.util.arrayList; //importando a classe ArrayList para poder fazer uso do vetor e armazenar elementos
+
+public class While {
+	/*Método principal da classe*/
+	public static void main (String [] args) {
+		//Fluxo de Repetição ussnado WHILE
+		int i = 0; //a variável i recebe o valor zero
+		while (i < 2) { //Enqanto i for menor que 2
+			System.out.println(i);
+			i++; //incremento da variável i
+		}
+	}//fim do método
+}//fim da classe
+
+/**
+while é utilizada quando desejamos executar esses laços em determinado bloco, enquanto a condição for verdadeira
+**/
